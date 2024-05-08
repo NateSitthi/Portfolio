891431062c7c2cd69a56830a07ca57d1cb95ca50
NateSitthi <143848445+NateSitthi@users.noreply.github.com>
2024-05-08 14:49:21 -0700

    5/8

--- java.js
+++ java.js
@@ -4,12 +4,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
 function openSidebar() {
     document.getElementById('sidebar').style.display = "flex";
     let animateleft = document.getElementById('sidebar')
-    animateleft.classList.remove("w3-animate-right")
     animateleft.classList.add("w3-animate-left")
 }
 function closeSidebar() {
     document.getElementById('sidebar').style.display = "none";
     let animateleft = document.getElementById('sidebar')
     animateleft.classList.remove("w3-animate-left")
-    animateleft.classList.add("w3-animate-right")
 }
